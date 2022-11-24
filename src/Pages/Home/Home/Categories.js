d36a39560381583cafcd87a40f0ea29a0aeedcaf
laboneeaktar <labonipub@gmail.com>
2022-11-24 19:19:22 +0600

    install react Qurery and set

---
+++ src/Pages/Home/Home/Categories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="lg:my-10 m-5">
+      <h1 className="text-center text-3xl bg-gradient-to-r from-rose-600 via-blue-700 to-green-600 inline-block text-transparent bg-clip-text">
+        Book Category
+      </h1>
+    </div>
+  );
+};
+
+export default Categories;
