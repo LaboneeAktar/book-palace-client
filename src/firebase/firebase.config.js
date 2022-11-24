0475cd0ef9ed3a3598cc91abc86fd11860f54493
laboneeaktar <labonipub@gmail.com>
2022-11-24 09:29:42 +0600

    Set Titile and fonts

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdDqtAo8h4DyJL4tLVcIqMLOQNah9CFs8",
+  authDomain: "book-palace-99c50.firebaseapp.com",
+  projectId: "book-palace-99c50",
+  storageBucket: "book-palace-99c50.appspot.com",
+  messagingSenderId: "632515705877",
+  appId: "1:632515705877:web:eb79f2461e70e4fff9e21c",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
